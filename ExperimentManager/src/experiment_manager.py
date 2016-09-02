4735af05e39a67a0f2c0552278cc2a785046e80c
Lucas Lehnert <lucas.lehnert@mail.mcgill.ca>
2016-09-02 18:56:32 -0400

    Test

--- ExperimentManager/src/experiment_manager.py
+++ ExperimentManager/src/experiment_manager.py
@@ -97,7 +97,7 @@ class Experiment:
             print '\n\n' + qsubCmd + '\n'
 
 #            subprocess.Popen( ['. ~/.bashrc'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE )
-            subprocess.Popen( ['qsub', '-cwd', '-now', nowBit, '-b', 'y', '-o', logFile, '-e', errFile, '-V', jobCmds], shell=True, executable='/bin/bash', cwd='.' )
+            subprocess.Popen( ['-cwd', '-now', nowBit, '-b', 'y', '-o', logFile, '-e', errFile, '-V', jobCmds], shell=False, executable='qsub', cwd='.' )
             jobId += 1
 
             print 'done'
